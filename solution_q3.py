c80f33da718dfb84dbd54b6a450b15ce87363881
Abhinav Chavali <abhinavchavali12@gmail.com>
2026-09-20 14:04:43 -0400

    Finished code

--- solution_q3.py
+++ solution_q3.py
@@ -1,4 +1,5 @@
 import heapq
+from math import ceil
 
 def read_state(filename="input.txt"):
     with open(filename) as f:
@@ -117,8 +118,6 @@ def transition_cost_b(state1, state2):
     else:
         return 0
 
-
-# Create General Graph Search Alg for A* and UCS
 def general_graph_search(start, cost_fn, heuristic_fn = lambda s: 0):
     # Create state space graph. What is each action available at each state?
     fringe = []
@@ -156,10 +155,19 @@ def general_graph_search(start, cost_fn, heuristic_fn = lambda s: 0):
                 counter += 1
                 
     return ([], 0, node_expansions)
-    
-def ucs(start, cost_fn):
-    return general_graph_search(start, cost_fn)
 
+def h1(state):
+    return 2 * state[0] + state[1]
+
+
+def h2(state):
+    return ceil((2 * state[0] + state[1]) / 3)
+
+def h3(state):
+    raise NotImplementedError
+
+def astar(start, heuristic):
+    return general_graph_search(start, transition_cost_a, heuristic_fn = heuristic)
 
 def report(title, result):
     path, cost, expansions = result
@@ -172,5 +180,6 @@ def report(title, result):
 
 if __name__ == "__main__":
     start = read_state()
-    report("Q2.1 (UCS, cost model A)", ucs(start, transition_cost_a))
-    report("Q2.1 (UCS, cost model B)", ucs(start, transition_cost_b))
+    report("Q3.1 (Heuristic 1)", astar(start, h1))
+    report("Q3.1 (Heuristic 2)", astar(start, h2))
+    #report("Q3.1 (Heuristic 3)", astar(start, h3))
